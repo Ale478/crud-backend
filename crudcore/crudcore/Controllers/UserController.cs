8a6fef6496f27ae100edb3643bf62b9500e2f65a
ale478 <alemusicmolin@gmail.com>
2024-02-25 15:53:48 -0300

    Adds db connection

---
+++ crudcore/crudcore/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using crudcore.Models;
+
+
+namespace crudcore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly string connectionsql;
+
+        public UserController(IConfiguration config)
+        {
+            connectionsql = config.GetConnectionString("Connection");
+        }
+
+
+    }
+}
