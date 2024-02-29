adddfdd96b90a1ca3781d981cb31151b489c84e0
ale478 <alemusicmolin@gmail.com>
2024-02-29 19:42:01 -0400

    hotfix

--- crudcore/crudcore/Models/TStatus.cs
+++ crudcore/crudcore/Models/TStatus.cs
@@ -8,17 +8,17 @@ public partial class TStatus
 {
     public string IdStatus { get; set; }
 
-    public string? StatusName { get; set; }
+    public string StatusName { get; set; }
 
-    public string? StatusDescription { get; set; }
+    public string StatusDescription { get; set; }
 
-    public string? UserCreation { get; set; }
+    public string UserCreation { get; set; }
 
-    public DateTime? DateCreation { get; set; }
+    public DateTime DateCreation { get; set; }
 
-    public string? UserModification { get; set; }
+    public string UserModification { get; set; }
 
-    public DateTime? DateModification { get; set; }
+    public DateTime DateModification { get; set; }
 
     [JsonIgnore]
     public virtual ICollection<TUser> TUsers { get; set; } = new List<TUser>();
